944c321a91517c1d751cbe0497e4d0ab3ea92287
Abhishek Singh <abhishekpratap.singh@newgenapps.com>
2013-12-27 16:04:56 +0530

    Initial todo repository

---
+++ Aptana Studio 3 Workspace/todo/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Todo::Application.config.secret_key_base = '7ef1a270b7bc43c27c7877d976753e61f8c6d8f3e4bc68ab3a27a2eb91400aa9b38a619d998eab272de94e6bc59da8de6cf057efa1cc708837311c0f19d52ab3'
